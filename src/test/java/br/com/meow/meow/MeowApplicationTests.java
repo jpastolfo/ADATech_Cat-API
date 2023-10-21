8a9b93ef341824cfb40cb33a91d03385bf92f5c5
Axtolphyno <joao.astolfo@lnls.br>
2023-10-21 20:34:01 -0300

    Add SPRING files
    MEOW

---
+++ src/test/java/br/com/meow/meow/MeowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meow.meow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
